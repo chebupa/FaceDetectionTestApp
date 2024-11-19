f845bd703f07fbd5487bf779d780aabad64e4f4d
chebupa <aristarhsakulin@gmail.com>
2024-11-20 00:35:24 +0300

    targets for testing added

---
+++ VisioFaceUnitTests/VisioFaceUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  VisioFaceUnitTests.swift
+//  VisioFaceUnitTests
+//
+//  Created by aristarh on 20.11.2024.
+//
+
+import Testing
+
+struct VisioFaceUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
